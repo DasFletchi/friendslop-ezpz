91231890b8372ba4ea66b3c085caeb0ac5472874
Fletchi <juliusblohm@icloud.com>
2026-07-03 16:17:36 +0200

    ChatGPT said uninstall the server

--- scripts/global.gd
+++ scripts/global.gd
@@ -19,9 +19,12 @@ func _ready() -> void:
 		print("Steam initialization failed: " + str(INIT['verbal']) + " Shutting down...")
 		get_tree().quit()
 
-ONLINE = Steam.loggedOn()
-STEAM_ID = Steam.get_steam_id()
-STEAM_NAME = Steam.get_steam_persona_name()
-OWNED = Steam.is_subscribed()l #sounds a bit weird but the is_owned check doesnt allow free week ends/Steam Family, this one allows that. However im all for piracy so i wont check if the game is owned entertainment should be available to every1 even if they cant afford it.
+	ONLINE = Steam.loggedOn()
+	STEAM_ID = Steam.get_steam_id()
+	STEAM_NAME = Steam.get_steam_persona_name()
+	OWNED = Steam.is_subscribed() #sounds a bit weird but the is_owned check doesnt allow free week ends/Steam Family, this one allows that. However im all for piracy so i wont check if the game is owned entertainment should be available to every1 even if they cant afford it.
 
-#Steam DRM check
+	#Steam DRM check
+	if OWNED == false:
+		print("Steam DRM check failed: Game not owned.")
+		#get_tree().quit() #like said i will allow piracy if you cant afford it

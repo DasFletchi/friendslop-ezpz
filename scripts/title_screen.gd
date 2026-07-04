0e80fee2aeaf96bb49140f3ab8fe2695cc918986
Fletchi <juliusblohm@icloud.com>
2026-07-04 17:39:25 +0200

    Fixed some stuff with

---
+++ scripts/title_screen.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+func _on_singleplayer_button_pressed() -> void:
+	pass
+
+
+func _on_multiplayer_button_pressed() -> void:
+	pass
+
+
+func _on_enet_pressed() -> void:
+	pass
+
+
+func _on_enet_ip_submitted(new_text: String) -> void:
+	pass
+
+
+func _on_enet_host_pressed() -> void:
+	pass
+
+
+func _on_enet_join_pressed() -> void:
+	pass
+
+
+func _on_back_pressed() -> void:
+	pass

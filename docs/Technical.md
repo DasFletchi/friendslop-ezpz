dd40eda83988f5defbe23dcbe5bc5f45872b1677
Fletchi <juliusblohm@icloud.com>
2026-07-16 20:36:50 +0200

    wire up buttons and add a new technical.md page

---
+++ docs/Technical.md
@@ -0,0 +1,3 @@
+using this tutorial for basic multiplayer that is p2p: https://www.youtube.com/watch?v=n8D3vEx7NAE
+
+adding noray netfox later
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,16 @@
 extends Node3D
 
+@onready var temp_mp_menu: PanelContainer = $"Mutliplayer temp menu/tempMPMenu"
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+
+func _on_host_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_join_pressed() -> void:
+	pass # Replace with function body.
